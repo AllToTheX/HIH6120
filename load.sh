bfb93e009bf7aa1254c0112588b059fd4568833f
Allex Veldman <allexveldman@gmail.com>
2016-07-03 13:08:46 +0200

    added copy of .rules file

--- load.sh
+++ load.sh
@@ -1,5 +1,9 @@
 sudo make
 if [ $? == 0 ]; then
-sudo rmmod HIH6120-stub
-sudo insmod HIH6120-stub.ko
+	sudo rmmod HIH6120-stub
+	sudo cp HIH6120-stub.rules /etc/udev/
+	if [ ! -f /etc/udev/rules.d/010_HIH6120-stub.rules ] ; then
+		sudo ln -s /etc/udev/HIH6120-stub.rules /etc/udev/rules.d/010_HIH6120-stub.rules
+	fi
+	sudo insmod HIH6120-stub.ko
 fi
